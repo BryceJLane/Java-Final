6d0f28bf2786905b0865742ed9a097ff6bcccaab
BryceJLane <130322752+BryceJLane@users.noreply.github.com>
2023-06-11 22:05:09 -0700

    finished and tested

--- src/js/recipe.js
+++ src/js/recipe.js
@@ -1,35 +1,63 @@
-// RecipeFinder class
 class RecipeFinder {
     constructor(apiKey) {
         this.apiKey = apiKey;
-        this.baseUrl = "https://api.spoonacular.com/recipes/complexSearch";
+        this.baseUrl = "https://api.spoonacular.com/recipes";
     }
 
     async findRecipesByIngredients(ingredients) {
-        const url = `${this.baseUrl}?apiKey=${this.apiKey}&includeIngredients=${ingredients.join(',')}`;
+        const url = `${this.baseUrl}/complexSearch?apiKey=${this.apiKey}&includeIngredients=${ingredients.join(',')}`;
         try {
             const response = await fetch(url);
             if (!response.ok) throw new Error('API request failed');
             const data = await response.json();
-            this.displayRecipes(data.results);
+            if (data.results.length === 0) {
+                this.displayError('No recipes found for the given ingredients.');
+            } else {
+                this.getRecipeDetails(data.results[0].id); // Get details of the first recipe
+            }
         } catch (error) {
-            console.error(error);
+            this.displayError(error.message);
         }
     }
 
-    displayRecipes(recipes) {
-        recipes.forEach(recipe => {
-            console.log(`Title: ${recipe.title}`);
-            console.log(`Image: ${recipe.image}`);
-            console.log(`Used ingredients count: ${recipe.usedIngredientCount}`);
-            console.log(`Missed ingredients count: ${recipe.missedIngredientCount}`);
-            console.log('----');
-        });
+    async getRecipeDetails(id) {
+        const url = `${this.baseUrl}/${id}/information?apiKey=${this.apiKey}`;
+        try {
+            const response = await fetch(url);
+            if (!response.ok) throw new Error('API request failed');
+            const data = await response.json();
+            this.displayRecipe(data);
+        } catch (error) {
+            this.displayError(error.message);
+        }
+    }
+
+    displayRecipe(recipe) {
+        if (!recipe) return;
+        const results = document.getElementById('results');
+        results.innerHTML = `
+            <div class="card">
+                <img class="card-img-top" src="${recipe.image}" alt="${recipe.title}">
+                <div class="card-body">
+                    <h5 class="card-title">${recipe.title}</h5>
+                    <p class="card-text">${recipe.instructions}</p>
+                    <a href="${recipe.sourceUrl}" class="btn btn-primary" target="_blank">Go to recipe</a>
+                </div>
+            </div>
+        `;
+    }
+
+    displayError(message) {
+        const results = document.getElementById('results');
+        results.innerHTML = `<div class="alert alert-danger">${message}</div>`;
     }
 }
 
 // Instantiate the class with your API key
 const recipeFinder = new RecipeFinder('07b172f893d44c94b46e5e7f1b18cac0');
 
-// Call the method to find recipes by ingredients
-recipeFinder.findRecipesByIngredients(['tomatoes', 'onions']);
+// Set up event listener for the search button
+document.getElementById('search').addEventListener('click', () => {
+    const ingredients = document.getElementById('ingredients').value.split(',').map(ingredient => ingredient.trim());
+    recipeFinder.findRecipesByIngredients(ingredients);
+});
